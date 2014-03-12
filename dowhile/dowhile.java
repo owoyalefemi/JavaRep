edd0de0729517f3a03833f2751f770ff2b2782c8
owoyale <owoyalefemi@gmail.com>
2014-03-12 16:06:11 +0200

    a dowhile added

---
+++ dowhile/dowhile.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class CompoundInt {
+
+	
+	public static void main (String args[]){
+		int counter = 0;
+		
+		do{
+			System.out.println(counter);
+			counter++;
+				
+		}while(counter <= 10);
+		
+	}
+}
